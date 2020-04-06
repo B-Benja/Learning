e4d6667f229a23c69ed68a05f84101f6ae0282de
B-Benja <53045556+B-Benja@users.noreply.github.com>
2020-04-06 20:06:46 +0200

    add comments/doc strings

--- Python_Beginner/words.py
+++ Python_Beginner/words.py
@@ -1,10 +1,19 @@
-# example of a python code, that can run as module or script
-# fetches words from URL and prints them; one word per line
+ """ 
+ example of a python code, that can run as module or script
+ fetches words from URL and prints them; one word per line
+ """ 
+
 import sys
 from urllib.request import urlopen
 
 
 def fetch_words(url):
+    """ receive a list of words from a URL 
+    
+    Args: the URL of a txt document
+    
+    Returns: stringt with the words from the document
+    """
     story = urlopen(url)
     story_words = []
     for line in story:
@@ -15,10 +24,14 @@ def fetch_words(url):
     return story_words
 
 def print_items(items):
+    """ Print one item per line    """
     for item in items:
         print(word)
 
 def main(url):
+    """ print each word from a txt document / from a URL
+    
+    Args: URL """
     words = fetch_words(url)
     print_items(words)
 
